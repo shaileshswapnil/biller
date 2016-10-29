18ee3b9f4730da49f54df7af59850a96cb0e14e7
Avishek <avishekadhvaryu89@gmail.com>
2016-10-29 21:17:10 +0530

    changes program.cs to start with form1

--- WindowsFormsApplication1/WindowsFormsApplication1/Program.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Program.cs
@@ -16,7 +16,7 @@ namespace WindowsFormsApplication1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new RecordsManagement_Company());
+            Application.Run(new Form1());
         }
     }
 }
